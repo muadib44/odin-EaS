cf01fcf57f23bb2c610949164319c726da803099
muadib44 <kartikeyajangid54@gmail.com>
2026-05-31 11:23:05 +0000

    Write the code to generate the grid, make sure to set tje size of the square according to the width you set

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const container = document.querySelector('.container');
 
 function createGrid(size) {
-   container.innerHTML = ''; }
-   
+   container.innerHTML = ''; 
+   const fragmant = document.createDocumentFragment();
+   const squareSize = 500 / size;
+   const totalSquares = size * size;
+   for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        square.style.width = `${squareSize}%`;
+        square.style.height = `${squareSize}%`;
+        fragmant.appendChild(square);
+   }
+    container.appendChild(fragmant);
+}
