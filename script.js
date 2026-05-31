a4c7c831e578ca686ec377db433381ceb349c574
muadib44 <kartikeyajangid54@gmail.com>
2026-05-31 12:06:00 +0000

    Create a button to choose and change the size of the grid

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const container = document.querySelector('.container');
 function createGrid(size) {
    container.innerHTML = ''; 
    const fragmant = document.createDocumentFragment();
-   const squareSize = 500 / size;
    const totalSquares = size * size;
    for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-square');
-        square.style.width = `${squareSize}%`;
-        square.style.height = `${squareSize}%`;
+        square.style.width = `calc(100% / ${size})`;
+        square.style.height = `calc(100% / ${size})`;
+        square.style.flexGrow = '1';
+        square.style.flexShrink = '1';
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black';
+            square.style.backgroundColor = 'aqua';
         });
         fragmant.appendChild(square);
    }
     container.appendChild(fragmant);
 }
+
+createGrid(16);
